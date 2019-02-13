2414c959b36e575269e150e58a288b359f2a5b3e
Krista Gurney <gurneykri@gmail.com>
2019-02-12 19:17:03 -0700

    added imports

--- hw05.py
+++ hw05.py
@@ -4,11 +4,24 @@
 
 ###########################################
 # module: hw05.py
-# YOUR NAME
-# YOUR A#
+# Krista Gurney
+# A01671888
 ###########################################
 
-# place the imports necessary for your solution here
+from var import var
+from const import const
+from pwr import pwr
+from prod import prod
+from plus import plus
+from quot import quot
+from maker import make_const, make_pwr, make_pwr_expr, make_plus, make_prod, make_quot, make_e_expr, make_ln, make_absv
+from tof import tof
+from deriv import logdiff
+from deriv import deriv
+from deriv import ln_deriv
+from hw05 import solve_pdeq, solve_pdeq_with_init_cond
+import unittest
+import math
 
 
 
